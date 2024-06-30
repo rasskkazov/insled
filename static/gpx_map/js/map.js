48b58cdc581e99d9b6f31227278d3291153b7ee2
Bogdan <brasskazov@gmail.com>
2024-06-30 16:39:03 +0300

    feat: autorouting

--- static/gpx_map/js/map.js
+++ static/gpx_map/js/map.js
@@ -1,165 +1,185 @@
+// Интерактивная карта
+
+function debounce(func, ms) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), ms);
+  };
+}
+
 var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    osmAttrib =
+  osmAttrib =
     '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    osm = L.tileLayer(osmUrl, {
-        maxZoom: 18,
-        attribution: osmAttrib
-    }),
-    map = new L.Map('map', {
-        center: new L.LatLng(59.9375, 30.308611),
-        zoom: 13,
-        zoomControl: false
-    }),
-    drawnItems = L.featureGroup().addTo(map);
+  osm = L.tileLayer(osmUrl, {
+    maxZoom: 18,
+    attribution: osmAttrib,
+  }),
+  map = new L.Map("map", {
+    center: new L.LatLng(59.9375, 30.308611),
+    zoom: 13,
+    zoomControl: false,
+  }),
+  drawnItems = L.featureGroup().addTo(map);
 
 L.control
-    .layers({
-        'По-умолчанию': osm.addTo(map),
-        'Спутник': L.tileLayer(
-            "http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}", {
-                attribution: "google",
-            }
-        ),
-    }, {
-        'Отображать маршрут': drawnItems
-    }, {
-        position: "topright",
-        collapsed: false
-    })
-    .addTo(map);
+  .layers(
+    {
+      "По-умолчанию": osm.addTo(map),
+      Спутник: L.tileLayer(
+        "http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}",
+        {
+          attribution: "google",
+        }
+      ),
+    },
+    {
+      "Отображать маршрут": drawnItems,
+    },
+    {
+      position: "topright",
+      collapsed: false,
+    }
+  )
+  .addTo(map);
 
 var info = L.control();
 
 info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info');
-    this.update();
+  this._div = L.DomUtil.create("div", "info");
+  this.update();
 
-    if (!L.Browser.touch) {
-        L.DomEvent
-            .disableClickPropagation(this._div)
-            .disableScrollPropagation(this._div);
-    } else {
-        L.DomEvent.on(this._div, 'dblclick', L.DomEvent.stopPropagation);
-    }
+  if (!L.Browser.touch) {
+    L.DomEvent.disableClickPropagation(this._div).disableScrollPropagation(
+      this._div
+    );
+  } else {
+    L.DomEvent.on(this._div, "dblclick", L.DomEvent.stopPropagation);
+  }
 
-    return this._div;
+  return this._div;
 };
 
 info.update = function (props) {
-    console.log(props)
-    this._div.innerHTML = props ? (`<h5>${props.name}</h5  >` + 'Протяженность: ' + props.length + ' км') : "Маршрут не загружен"
+  console.log(props);
+  this._div.innerHTML = props
+    ? `<h5>${props.name}</h5  >` + "Протяженность: " + props.length + " км"
+    : "Маршрут не загружен";
 };
 
 info.addTo(map);
 
-L.control.zoom({
-    position: 'bottomleft'
-}).addTo(map);
+L.control
+  .zoom({
+    position: "bottomleft",
+  })
+  .addTo(map);
 
 map.addControl(
-    new L.Control.Draw({
-        edit: {
-            featureGroup: drawnItems,
-            poly: {
-                allowIntersection: true,
-            },
+  new L.Control.Draw({
+    edit: {
+      featureGroup: drawnItems,
+      poly: {
+        allowIntersection: true,
+      },
+    },
+    draw: {
+      poly: {
+        shapeOptions: {
+          color: "#00008b",
         },
-        draw: {
-            poly: {
-                shapeOptions: {
-                    color: '#00008b'
-                }
-            },
-
-            polygon: false,
-            circle: false,
-            circlemarker: false,
-            rectangle: false
-
-        },
-    })
+      },
+      circlemarker: false,
+      polygon: false,
+      circle: false,
+      rectangle: false,
+    },
+  })
 );
 
+var routingControl = L.Routing.control({
+  waypoints: [],
+  createMarker: function () {
+    return null;
+  },
+  routeWhileDragging: true,
+  addWaypoints: false,
+  show: false,
+}).addTo(map);
+const markers = new Map();
+let autoRouteIdCounter = 1;
+
 map.on(L.Draw.Event.CREATED, function (event) {
-    var layer = event.layer;
+  var layer = event.layer;
 
-    if (event.layerType === 'marker') {
+  if (event.layerType === "marker") {
+    layer.addEventListener("dblclick", function (e) {
+      var name = "";
+      var desc = "";
 
-        layer.addEventListener('dblclick', function (e) {
+      $("#editpopup").modal();
+      document.getElementById("edit-popup-modal-input-name").value = name;
+      document.getElementById("edit-popup-modal-input-desc").value = desc;
+      currentPopup = layer;
+    });
 
-            var name = ''
-            var desc = ''
+    layer.options.autoId = autoRouteIdCounter++;
 
-            $("#editpopup").modal()
-            document.getElementById('edit-popup-modal-input-name').value =
-                name
-            document.getElementById('edit-popup-modal-input-desc').value =
-                desc
-            currentPopup = layer
+    markers.set(layer.options.autoId, layer.getLatLng());
+    routingControl.setWaypoints([...markers.values()]);
+  }
 
-        });
-    }
+  drawnItems.addLayer(layer);
+});
 
-    drawnItems.addLayer(layer);
+const debouncedUpdateOnMove = debounce((layer) => {
+  markers.set(layer.options.autoId, layer.getLatLng());
+  routingControl.setWaypoints([...markers.values()]);
+}, 250);
+
+map.on(L.Draw.Event.EDITMOVE, function (event) {
+  const layer = event.layer;
+  if (Object.hasOwn(layer.options, "autoId")) {
+    debouncedUpdateOnMove(layer);
+  }
+});
+
+map.on(L.Draw.Event.DELETED, function (event) {
+  event.layers.eachLayer((layer) => {
+    markers.delete(layer.options.autoId);
+  });
+  routingControl.setWaypoints([...markers.values()]);
 });
 
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 
-// document.getElementById('saveAndUpload').onclick = function (e) {
-//     // Extract GeoJson from featureGroup
-//     var data = drawnItems.toGeoJSON();
+document.getElementById("export").onclick = function (e) {
+  var data = drawnItems.toGeoJSON();
+  console.log(
+    "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data))
+  );
 
-//     // Stringify the GeoJson
-//     var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+  var gpxData = GeoJsonToGpx(data);
 
-//     // Convert to .gpx
+  var serializer = new XMLSerializer();
+  var gpxString = serializer.serializeToString(gpxData);
+  var blob = new Blob([gpxString], { type: "application/gpx+xml" });
+  var url = URL.createObjectURL(blob);
 
-//     // Create export
-//     let csrf_token = getCookie('csrftoken')
-
-//     $.ajax({
-//         headers: {
-//             "X-CSRFToken": csrf_token,
-//             'CSRFToken': csrf_token
-//         },
-//         type: "POST",
-//         contentType: 'application/json; charset=utf-8',
-//         data: JSON.stringify(data),
-//         dataType: 'text',
-//         success: function (result) {
-//             alert(result.Result);
-//         }
-//     });
-
-//     document.getElementById('export').setAttribute('href', 'data:' + convertedData);
-//     document.getElementById('export').setAttribute('download', 'data.geojson');
-// }
-
-document.getElementById('export').onclick = function (e) {
-    console.log('Exporting')
-    // Extract GeoJson from featureGroup
-    var data = drawnItems.toGeoJSON();
-
-    // Stringify the GeoJson
-    var convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
-
-    // Convert to .gpx
-
-    // Create export
-    document.getElementById('export').setAttribute('href', 'data:' + convertedData);
-    document.getElementById('export').setAttribute('download', 'data.geojson');
-}
+  document.getElementById("export").setAttribute("href", url);
+  document.getElementById("export").setAttribute("download", "data.gpx");
+};
